c04ff5621cc8736cfbbbbe127832ed6695eabeca
Fleur Kelpin <f.kelpin@umcg.nl>
2020-07-14 16:23:44 +0200

    refactor: use molgenis-r-auth package

--- R/auth.R
+++ R/auth.R
@@ -1,33 +1,46 @@
 #' Login to the Armadillo MinIO filestore
 #'
-#' @param server url of the MinIO server
 #' @param token oAuth ID token
+#' @param server url of the MinIO server
 #' @param duration duration in seconds
+#' @param use boolean indicating if the credentials should be set
 #'
-#' @importFrom httr POST stop_for_status content oauth_endpoint
-#' @importFrom xml2 xml_ns_strip xml_text xml_find_first
+#' @importFrom aws.iam set_credentials
+#' @importFrom httr POST stop_for_status content
+#' @importFrom xml2 xml_text xml_find_first xml_ns_strip
 #'
 #' @export
-login <- function(server, token, duration = 3600) {
-  response <- httr::POST(server,
-    query = list(
+assume_role_with_web_identity <-
+  function(token,
+           server = Sys.getenv("AWS_S3_ENDPOINT"),
+           duration = 900,
+           use = TRUE) {
+    if (duration < 900 | duration > 129600) {
+      stop("'duration' must be a value in seconds between 900 and 129600")
+    }
+    query <- list(
       Action = "AssumeRoleWithWebIdentity",
       DurationSeconds = duration,
       WebIdentityToken = token,
       Version = "2011-06-15"
     )
-  )
-  httr::stop_for_status(response, "assume role on MinIO")
-  content <- xml2::xml_ns_strip(httr::content(response))
+    response <- httr::POST(url = server, query = query)
+    httr::stop_for_status(response, "assume role on S3 server")
+    content <- xml2::xml_ns_strip(httr::content(response))
 
-  # TODO: Shouldn't these be in a session object of sorts?
-  return(list(
-    key = xml2::xml_text(xml2::xml_find_first(content, "//AccessKeyId")),
-    secret = xml2::xml_text(xml2::xml_find_first(content, "//SecretAccessKey")),
-    session_token =
-      xml2::xml_text(xml2::xml_find_first(content, "//SessionToken"))
-  ))
-}
+    credentials <- list(
+      AccessKeyId =
+        xml2::xml_text(xml2::xml_find_first(content, "//AccessKeyId")),
+      SecretAccessKey =
+        xml2::xml_text(xml2::xml_find_first(content, "//SecretAccessKey")),
+      SessionToken =
+        xml2::xml_text(xml2::xml_find_first(content, "//SessionToken"))
+    )
+    if (isTRUE(use)) {
+      aws.iam::set_credentials(credentials)
+    }
+    invisible(credentials)
+  }
 
 #' Get ID Token
 #'
@@ -37,15 +50,14 @@ login <- function(server, token, duration = 3600) {
 #'
 #' @return The ID token string
 #'
-#' @importFrom urltools path
-#' @importFrom utils browseURL
-#' @importFrom httr GET RETRY POST stop_for_status content
+#' @importFrom MolgenisAuth discover device_flow_auth
+#' @importFrom httr GET stop_for_status content
 #'
 #' @export
 get_token <- function(server) {
   auth_info <- .get_info(server)$auth
-  endpoint <- .discover_endpoint(auth_info$issuerUri)
-  credentials <- .device_flow_token(
+  endpoint <- MolgenisAuth::discover(auth_info$issuerUri)
+  credentials <- MolgenisAuth::device_flow_auth(
     endpoint,
     auth_info$clientId
   )
@@ -59,65 +71,3 @@ get_token <- function(server) {
   httr::stop_for_status(response, task = "fetch server info")
   return(httr::content(response))
 }
-
-.discover_endpoint <- function(auth_server) {
-  openid_config_url <- auth_server
-  urltools::path(openid_config_url) <- ".well-known/openid-configuration"
-  response <- httr::GET(openid_config_url)
-  httr::stop_for_status(response, task = "discover OpenID configuration")
-  configuration <- httr::content(response)
-
-  return(httr::oauth_endpoint(
-    request = NULL,
-    authorize = configuration$authorization_endpoint,
-    access = configuration$token_endpoint,
-    user = configuration$userinfo_endpoint,
-    device = configuration$device_authorization_endpoint,
-    logout = configuration$end_session_endpoint
-  ))
-}
-
-.device_flow_token <-
-  function(endpoint, client_id) {
-    stopifnot(
-      inherits(endpoint, "oauth_endpoint"),
-      is.character(client_id),
-      is.character(endpoint$device)
-    )
-    response <- httr::POST(endpoint$device,
-      body = list(
-        client_id = client_id,
-        scope = "openid offline_access"
-      )
-    )
-    httr::stop_for_status(response,
-                          task = "initiate OpenID Device Flow authentication")
-    auth_res <- httr::content(response)
-    print(paste0(
-      "We're opening a browser so you can log in with code ",
-      auth_res$user_code
-    ))
-    verification_url <- auth_res$verification_uri_complete
-    verification_url <- urltools::param_set(
-      verification_url,
-      "client_id", client_id
-    )
-    utils::browseURL(verification_url)
-
-    response <- httr::RETRY(
-      url = endpoint$access,
-      verb = "POST",
-      pause_base = auth_res$interval,
-      pause_cap = auth_res$interval,
-      pause_min = auth_res$interval,
-      times = auth_res$expires_in / auth_res$interval,
-      quiet = FALSE,
-      body = list(
-        "client_id" = client_id,
-        "grant_type" = "urn:ietf:params:oauth:grant-type:device_code",
-        "device_code" = auth_res$device_code
-      )
-    )
-    httr::stop_for_status(response, task = "retrieve id token")
-    return(httr::content(response))
-  }
--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -1,38 +1,48 @@
-test_that("login retrieves session info", {
+test_that("assume_role_with_webidentity_retrieves_credentials", {
   response <- structure(list(status_code = 200), class = "response")
   httr_post <- mock(response)
-  content <- xml2::as_xml_document(list(AssumeRoleWithWebIdentityResponse = structure(list(
-    AssumeRoleWithWebIdentityResult = list(
-      AssumedRoleUser = list(Arn = list(), AssumeRoleId = list()),
-      Credentials = list(
-        AccessKeyId = list("UNWUHGSS9OOE1L7I49X9"),
-        SecretAccessKey = list("TiAe4MEuwgU+urcM94kLEORsk8PZtu+JJ6mmmOAj"),
-        Expiration = list("2020-07-10T10:55:53Z"), SessionToken = list("edcba")
-      ),
-      SubjectFromWebIdentityToken = list("87ff7211-5050-4565-9137-f2547207c852")
-    ),
-    ResponseMetadata = list(RequestId = list("16205C5CE86AE656"))
-  ), xmlns = "https://sts.amazonaws.com/doc/2011-06-15/")))
+  content <- xml2::as_xml_document(
+    list(
+      AssumeRoleWithWebIdentityResponse =
+        structure(list(
+          AssumeRoleWithWebIdentityResult = list(
+            AssumedRoleUser = list(Arn = list(), AssumeRoleId = list()),
+            Credentials = list(
+              AccessKeyId = list("UNWUHGSS9OOE1L7I49X9"),
+              SecretAccessKey =
+                list("TiAe4MEuwgU+urcM94kLEORsk8PZtu+JJ6mmmOAj"),
+              Expiration = list("2020-07-10T10:55:53Z"),
+              SessionToken = list("edcba")
+            ),
+            SubjectFromWebIdentityToken =
+              list("87ff7211-5050-4565-9137-f2547207c852")
+          ),
+          ResponseMetadata = list(RequestId = list("16205C5CE86AE656"))
+        ), xmlns = "https://sts.amazonaws.com/doc/2011-06-15/")
+    )
+  )
 
   httr_content <- mock(content)
 
   with_mock(
-    session <- login("https://example.org", "abcde", 120),
+    session <- assume_role_with_web_identity(
+      "abcde", "https://example.org", 900
+    ),
     "httr::POST" = httr_post,
     "httr::content" = httr_content
   )
 
   expect_equal(session, list(
-    key = "UNWUHGSS9OOE1L7I49X9",
-    secret = "TiAe4MEuwgU+urcM94kLEORsk8PZtu+JJ6mmmOAj",
-    session_token = "edcba"
+    AccessKeyId = "UNWUHGSS9OOE1L7I49X9",
+    SecretAccessKey = "TiAe4MEuwgU+urcM94kLEORsk8PZtu+JJ6mmmOAj",
+    SessionToken = "edcba"
   ))
 
   expect_args(httr_post, 1,
     "https://example.org",
     query = list(
       Action = "AssumeRoleWithWebIdentity",
-      DurationSeconds = 120,
+      DurationSeconds = 900,
       WebIdentityToken = "abcde",
       Version = "2011-06-15"
     )
