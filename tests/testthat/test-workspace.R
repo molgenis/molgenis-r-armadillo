82d3bf91cde39b2444566116c87f3aee0410b594
Fleur Kelpin <fleur@kelpin.nl>
2020-07-22 11:17:26 +0200

    Update tests/testthat/test-workspace.R
    Co-authored-by: Tommy de boer <tommydeboer4@gmail.com>

--- tests/testthat/test-workspace.R
+++ tests/testthat/test-workspace.R
@@ -243,7 +243,7 @@ test_that("armadillo.load_workspace checks if the folder exists", {
       "aws.s3::bucket_exists" = bucket_exists,
       "MolgenisArmadillo:::.use_https" = mock(TRUE)
     ),
-    "Folder 'example' doesnot exist\\."
+    "Folder 'example' does not exist\\."
   )
 
   expect_args(bucket_exists, 1, "shared-example", use_https = TRUE)
