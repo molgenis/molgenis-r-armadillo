3e5622cb168679ac6339f6eb9cee34303365e479
sidohaakma <sido@haakma.org>
2020-09-07 15:06:58 +0200

    chore: updat vignettes with additional methods

--- vignettes/pre-render.R
+++ vignettes/pre-render.R
@@ -1,3 +1,2 @@
 knitr::knit("vignettes/MolgenisArmadillo.Rmd.orig", output = "vignettes/MolgenisArmadillo.Rmd")
-knitr::purl("vignettes/MolgenisArmadillo.Rmd.orig", output = "vignettes/MolgenisArmadillo.R")
 
