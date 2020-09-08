68d5c167483fc2b043a79860ddb7ce09a40b9304
Fleur Kelpin <f.kelpin@umcg.nl>
2020-09-08 12:58:19 +0200

    chore: update pre-render script

--- vignettes/pre-render.R
+++ vignettes/pre-render.R
@@ -1,2 +1,8 @@
-knitr::knit("vignettes/MolgenisArmadillo.Rmd.orig", output = "vignettes/MolgenisArmadillo.Rmd")
+# Set work directory (N.B. user specific!)
+setwd("~/git/molgenis-r-armadillo/vignettes")
 
+# To update the vignette:
+knitr::knit("MolgenisArmadillo.Rmd.orig", output = "MolgenisArmadillo.Rmd")
+
+# To create a script that uploads the files:
+knitr::purl("MolgenisArmadillo.Rmd.orig", output = "MolgenisArmadillo.R")
