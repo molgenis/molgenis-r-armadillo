31d5f1fe99580fd31a678d6be9e6b13263ea5833
Sido Haakma <sido@haakma.org>
2022-02-24 09:09:20 +0100

    feat: puth authentication context in R package options

--- R/package.R
+++ R/package.R
@@ -4,31 +4,40 @@
 #'
 #' @param path the path to the package
 #' @param profile the selected profile
-#' @param url the url of the armadillo service
 #'
 #' @export
-armadillo.install_package <- function(path, profile="default", url) {
-  token <- armadillo.get_token(url)
-  headers <- httr::add_headers("Authorization" = paste0("Bearer ", token))
+armadillo.install_packages <- function(path, profile="default") {
 
-  handle <- httr::handle(url)
+  if(missing(path) || path == "") {
+    stop("You need to specify the full path of the package; e.g. 'C:/User/test.tar.gz'")
+  }
+  
+  auth_token <- getOption("MolgenisArmadillo.auth.token", TRUE)
+  auth_endpoint <- getOption("MolgenisArmadillo.auth.endpoint", TRUE)
+  
+  if(is.null(auth_endpoint) || is.null(auth_token)) {
+    stop("Please login using; 'armadillo.login('http://armadillo', 'http://minio')'")
+  }
+  
+  headers <- httr::add_headers("Authorization" = paste0("Bearer ", auth_token))
+  
+  handle <- httr::handle(auth_endpoint)
   
   file <- httr::upload_file(path)
-  
-  query <- list(
-    profile = profile
-  )
+  if(!file.exists()) {
+    stop("Please specificy a valid path for your package.")
+  }
   
   response <- httr::POST(
     handle = handle,
-    query = query,
+    query = list(profile = profile),
     path = "/install-package",
     body = file,
     config = c(headers, httr::content_type("multipart"))
   )
   .handle_request_error(response)
   if (response$status == 404) {
-    stop(paste0("Profile not found: '", profile, "'"))
+    stop("Something went wrong")
   }
   
 }
