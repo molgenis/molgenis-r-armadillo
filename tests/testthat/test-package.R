cb1fb60ebc2c4275d4bae8bb5a620407c87b5646
Sido Haakma <sido@haakma.org>
2022-03-10 16:43:50 +0100

    tests: test connection

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -1,3 +1,25 @@
+test_that("failed to get armadillo connection", {
+  expect_error(
+    .get_armadillo_connection(),
+    "Please login using: 'armadillo.login('http://armadillo', 'http://minio')'",
+    fixed=TRUE
+  )
+})
+
+test_that("get armadillo connection", {
+  options(MolgenisArmadillo.auth.token = "abcde")
+  options(MolgenisArmadillo.armadillo.endpoint = "http://armadillo")
+  
+  local_reproducible_output()
+  expect_equal(
+    .get_armadillo_connection(),
+    list(handle = httr::handle("http://armadillo"), headers = httr::add_headers("Authorization" = "Bearer abcde"))
+  )
+  
+  options(MolgenisArmadillo.auth.token = NULL)
+  options(MolgenisArmadillo.armadillo.endpoint = NULL)
+})
+
 test_that("failed to install a package", {
   response <- list(status_code = 500)
   httr_post <- mock(response, cycle = TRUE)
@@ -15,6 +37,26 @@ test_that("failed to install a package", {
       "httr:::content" = httr_content
     ),
     paste0("Internal server error: ")
+  )
+})
+
+test_that("failed to install a package", {
+  response <- list(status_code = 404)
+  httr_post <- mock(response, cycle = TRUE)
+  httr_handle <- mock(handle)
+  httr_content <- mock("Could not install package")
+  connection <- mock(list(handle = httr_handle, headers = mock()))
+  expected_path <- "C:/test/test.tar.gz"
+  
+  expect_error(
+    with_mock(
+      .install_package(path = expected_path),
+      "MolgenisArmadillo:::.get_armadillo_connection" = connection,
+      "httr:::upload_file" = mock(),
+      "httr:::POST" = httr_post,
+      "httr:::content" = httr_content
+    ),
+    paste0("")
   )
 })
 
