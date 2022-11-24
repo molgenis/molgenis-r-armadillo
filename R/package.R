8af9d128547e0aa373b135ac62a88fbc71c4756f
tommy <tommydeboer4@gmail.com>
2022-11-24 14:05:23 +0100

    chore: remove whitelist function (not needed)

--- R/package.R
+++ R/package.R
@@ -1,6 +1,8 @@
 #' Install package
 #'
-#' Installs a user defined package.
+#' Installs a user defined package into the provided profile. The package
+#' is automatically whitelisted after installation. Only available during
+#' development.
 #'
 #' @param paths the path(s) to the package(s), can be a vector or a string
 #' @param profile the selected profile
@@ -55,74 +57,6 @@ armadillo.install_packages <- function(paths, profile = "default") { # nolint
   .handle_request_error(response)
 
   message(paste0("Package [ '", path, "' ] installed"))
-}
-
-#' Whitelist packages
-#'
-#' When you installed developer packages you need to whitelist
-#' them in other to be able to test them in the Armadillo platform.
-#'
-#' @param pkgs packages that need to be added to the whitelist
-#' @param profile the profile you want to whitelist the packages on
-#'
-#' @export
-armadillo.whitelist_packages <- function(pkgs, profile = "default") { # nolint
-  msg <- paste0(
-    "You need to specify the the package(s) you want to whitelist; ",
-    "e.g. 'DSI'")
-  .is_empty(msg, pkgs)
-
-  response <- httr::POST(
-    url = .get_url(),
-    path = "/select-profile",
-    body = profile,
-    config = httr::add_headers(.get_auth_header())
-  )
-
-  if (response$status_code == 404 && profile != "default") {
-    stop(paste0("Profile not found: [ '", profile, "' ]"))
-  }
-
-  .handle_request_error(response)
-
-  invisible(lapply(pkgs, .whitelist_package))
-
-  response <- httr::GET(
-    handle = .get_url(),
-    path = "/whitelist",
-    config = httr::add_headers(.get_auth_header())
-  )
-
-  .handle_request_error(response)
-
-  message(paste0("Packages whitelisted: "))
-  message(paste(
-    paste0(
-      " * [ '", httr::content(response), "' ]",
-      collapse = "\n")))
-}
-
-#' Add package to whitelist
-#'
-#' @param pkg package to whitelist
-#'
-#' @noRd
-.whitelist_package <- function(pkg) {
-  message(paste0("Attempting to whitelist package [ '", pkg, "' ]"))
-  response <- httr::POST(
-    handle = .get_url(),
-    path = paste0("/whitelist/", pkg),
-    config = httr::add_headers(.get_auth_header())
-  )
-
-  if (response$status_code == 404) {
-    stop(paste0(
-      "Endpoint doesn't exist. ",
-      "Make sure you're running Armadillo in development mode."))
-  }
-
-  .handle_request_error(response)
-  message(paste0("Package [ '", pkg, "' ] added to the whitelist"))
 }
 
 #' Checks for empty list or character
