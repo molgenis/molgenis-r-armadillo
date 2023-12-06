a7297d30de95681a35eadeb63c69c7bf479252f7
Clemens Tolboom <c.g.w.m.tolboom@umcg.nl>
2023-12-06 13:09:09 +0100

    Move code into script and ignore artefacts.

---
+++ dev/bin/check-and-build.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -x
+          export PACKAGE=`grep Package DESCRIPTION | head -n1 | cut -d':' -f2 | xargs`
+          export TAG=`grep Version DESCRIPTION | head -n1 | cut -d':' -f2 | xargs` 
+          echo "Testing ${PACKAGE}:${TAG}"
+          Rscript -e 'devtools::check(remote=TRUE, force_suggests = TRUE, error_on="error")'
+          Rscript -e 'library(covr);codecov()'
+          R CMD build .
+          Rscript -e "devtools::check_built(path = './${PACKAGE}_${TAG}.tar.gz', remote=TRUE, force_suggests = TRUE)"
+          Rscript -e "pkgdown::build_site()"
