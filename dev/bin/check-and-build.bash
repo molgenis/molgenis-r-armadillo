e4c8127d90e42d5e0f688caaf8c17594def4a5bb
mkslofstra <mariska-karina@hotmail.com>
2024-10-01 15:38:18 +0200

    chore: remove chaos

--- dev/bin/check-and-build.bash
+++ dev/bin/check-and-build.bash
@@ -9,4 +9,3 @@ Rscript -e 'devtools::check(remote=TRUE, force_suggests = TRUE, error_on="error"
 Rscript -e 'library(covr);codecov()'
 R CMD build .
 Rscript -e "devtools::check_built(path = './${PACKAGE}_${TAG}.tar.gz', remote=TRUE, force_suggests = TRUE)"
-Rscript -e "pkgdown::build_site()"
